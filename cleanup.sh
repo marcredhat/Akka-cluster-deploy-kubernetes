7c166a83c93983c2bcf5c755f22ca336a5b8736c
Marc Chisinevski <mchisine@redhat.com>
2020-08-23 15:22:44 -0700

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,5 @@
 sudo rm -rf ./Akka-cluster-deploy-kubernetes
 oc delete statefulset,svc --all -n akka
 oc delete pod -l app=myapp --force --grace-period 0
+oc delete  -f https://raw.githubusercontent.com/marcredhat/Akka-cluster-deploy-kubernetes/master/deploy/kubernetes/resources/myapp/statefulset-with-pull-secret.yaml
+oc delete  -f https://raw.githubusercontent.com/marcredhat/Akka-cluster-deploy-kubernetes/master/deploy/kubernetes/resources/myapp/myapp-service.yaml
