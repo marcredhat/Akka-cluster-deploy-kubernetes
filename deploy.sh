e57a7ad76a844af8a3c60219829bea4ae221b8f2
Marc Chisinevski <mchisine@redhat.com>
2020-08-23 15:26:09 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+
+
+oc create secret docker-registry marcsecretquay --docker-server=quay.io --docker-username=$quayusername --docker-password=$quayencryptedpassword --docker-email=$quayemail
 oc create  -f https://raw.githubusercontent.com/marcredhat/Akka-cluster-deploy-kubernetes/master/deploy/kubernetes/resources/myapp/statefulset-with-pull-secret.yaml
 oc create  -f https://raw.githubusercontent.com/marcredhat/Akka-cluster-deploy-kubernetes/master/deploy/kubernetes/resources/myapp/myapp-service.yaml
 oc expose svc my-app
