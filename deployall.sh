3184996c173495ae250e000d8305802c029f9bca
Marc Chisinevski <mchisine@redhat.com>
2020-08-23 15:40:27 -0700

    Update deployall.sh

--- deployall.sh
+++ deployall.sh
@@ -1,3 +1,7 @@
+oc version 
+
+sleep 4
+
 git clone https://github.com/marcredhat/Akka-cluster-deploy-kubernetes.git
 
 cd Akka-cluster-deploy-kubernetes
@@ -5,3 +9,15 @@ cd Akka-cluster-deploy-kubernetes
 sudo chmod +x *.sh
 
 ./cleanup.sh
+
+sleep 3
+
+oc get pods
+
+sleep 4
+
+./buildandpushimage.sh
+
+./deploy.sh
+
+./generatetraffic.sh
